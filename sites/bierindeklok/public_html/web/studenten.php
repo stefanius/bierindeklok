a3bdea89208d59acaaedc5616f3aa31d6edae4cf
stefanius <KIT_kat_16>
2014-12-22 21:31:38 +0100

    Updated BIDK

---
+++ sites/bierindeklok/public_html/web/studenten.php
@@ -0,0 +1,112 @@
+<?php $currenttime = date("H:i"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Wanneer mag het pils gedronken worden? Het antwoord komt uit de Studentenwereld! Want staat in de klok een vier, dan drinken wij het Bier!">
+
+    <title>Bier in de klok?</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/cover.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">BierInDeKlok.nl</h3>
+              <ul class="nav masthead-nav">
+                <li class="active"><a href="/">Home</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Studenten</h1>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- bierindeklok -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-1143505562670221"
+     data-ad-slot="5087446017"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+            <p class="lead">Ieder mens weet dat een student wel raad weet met een pilsje. Maar nu zijn er allemaal regels wanneer dat biertje nu mag. Gelukkig zijn ze niet al te ingewikkeld, want
+als de 4 in de klok staat, mag het flesje gerstenat ontdaan worden van zijn kroonkurk.</p>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- bierindeklok -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-1143505562670221"
+     data-ad-slot="5087446017"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
+            <p class="lead">Ieder mens weet dat een student wel raad weet met een pilsje. Maar nu zijn er allemaal regels wanneer dat biertje nu mag. Gelukkig zijn ze niet al te ingewikkeld, want
+als de 4 in de klok staat, mag het flesje gerstenat ontdaan worden van zijn kroonkurk.</p>
+
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- bierindeklok -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-1143505562670221"
+     data-ad-slot="5087446017"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+        
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-36212308-4', 'bierindeklok.nl');
+  ga('send', 'pageview');
+
+</script>
+
